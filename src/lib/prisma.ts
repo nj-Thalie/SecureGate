535d00614000f3cf0cd2fdc5e3d0b23f6d26354e
Nephtalie Joseph <nj.nephtaliejoseph@gmail.com>
2026-05-20 17:14:02 -0400

    COMMIT

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.DATABASE_URL ?? '',
 });
 
 declare global {
